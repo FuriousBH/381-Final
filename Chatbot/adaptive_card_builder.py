ce5ee35e74edcb14ab2e1dbd4c99abc363104801
thecolorpuce <rileyasp@gmail.com>
2022-11-21 05:46:35 +0000

    Added functionality in preparation for dynamic cards

---
+++ Chatbot/adaptive_card_builder.py
@@ -0,0 +1,9 @@
+from webexteamsbot import TeamsBot
+from webexteamsbot.models import Response
+from adaptivecardbuilder import *
+import time
+import json
+import requests
+import mod_skills as usefulP
+
+
